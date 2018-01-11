55d73081e65f04ac41479890cb330dcf17b4c812
ZhenWei <zhenwei@ZhenWeideMacBook-Pro.local>
2018-01-11 18:25:28 +0800

    Add new function
    For trim galore.

---
+++ R/Rtrim_galore.R
@@ -0,0 +1,51 @@
+#' @title generate a command for trim_galore in R
+#' @examples
+#' arg_df <- data.frame(
+#' Fastq_file_name = rep("SRR1744126",3), 
+#' trim_5 = c(16,16,0),
+#' trim_3 = c(12,0,12),
+#' output_name = paste0("SRR1744126_",1:3)
+#' )
+#' 
+#' for(i in 1:nrow(arg_df)) {
+#' x = arg_df[i,]
+#' Rhisat2(Fastq_file_name = x$Fastq_file_name,
+#'         trim_5 = x$trim_5,
+#'         trim_3 = x$trim_3,
+#'         output_name = x$output_name,
+#'         Fastq_directory = "/home/zhen/TREW_new_data") %>% Rnohup(.,paste0(x$output_name,"_trim") )
+#' }
+#' 
+#' @import dplyr
+#' 
+#' @export
+#' 
+#' trim_galore --quality 20 --phred33 --fastqc --paired --clip_R1 0 --clip_R2 0 --three_prime_clip_R1 0 --three_prime_clip_R2 0 SRR1234567_1.fq SRR1234567_2.fq
+
+Rtrim_galore <-
+  function(GEO_accession = "SRR1234567", 
+            fastq_end = ".fastq",
+            Paired = F, 
+            Phred_cut = 20, 
+            Phred = 33, 
+            fastqc = T, 
+            CLIP_5 = c(0,0),
+            CLIP_3 = c(0,0)) { 
+    
+   if (length(CLIP_5) == 1) CLIP_5 = c(CLIP_5,CLIP_5)
+   if (length(CLIP_3) == 1) CLIP_3 = c(CLIP_3,CLIP_3)
+   
+   if(Paired == F){
+   Paired_dependent_chunk <- paste0("--clip_R1 ",CLIP_5[1]," --three_prime_clip_R1 ",CLIP_3[1]," ", GEO_accession,fastq_end)
+   }else{
+   Paired_dependent_chunk <- paste0("--clip_R1 ",CLIP_5[1],
+                                    " --clip_R2 ",CLIP_5[2],
+                                    " --three_prime_clip_R1 ",CLIP_3[1],
+                                    " --three_prime_clip_R2 ",CLIP_3[2],
+                                    " ", GEO_accession,"_1",fastq_end,
+                                    " ", GEO_accession,"_2",fastq_end)
+   }
+    
+   paste0("trim_galore ","--quality ",Phred_cut," --phred",Phred,ifelse(fastqc," --fastqc "," "),Paired_dependent_chunk)
+
+}
--- R/WgetQC.R
+++ R/WgetQC.R
@@ -1,26 +1,34 @@
 #' @title Retrive data from GEO, and run a fastqc
 #' 
 #' @examples
-#' sapply(paste0("SRR",3066062:3066069),function(x) nohup_R(WgetQC(x),x))
+#' sapply(paste0("SRR",3066062:3066069),function(x) Rnohup(WgetQC(x),x,Paired = F))
 #' 
 #' @export
 
 WgetQC <-
 function (GEO_accession = "SRR1234567", Paired = F) 
 {
+  
   URL = paste("ftp://ftp-trace.ncbi.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR", 
-              substr(GEO_accession, 1, 6), GEO_accession, paste0(GEO_accession, 
-                                                                 ".sra"), sep = "/")
+                    substr(GEO_accession, 1, 6), 
+                           GEO_accession, paste0(GEO_accession,".sra"), sep = "/")
+  
   Wget_command = paste("wget -c -t 0", URL)
+  
   fastq_dump_command = paste0("fastq-dump ", ifelse(Paired, 
                                                     "--split-3 ", ""), GEO_accession, ".sra")
+  
   if (Paired) {
     fastqc_command = paste0("fastqc ", GEO_accession, "_1.fastq", 
                             ";", "fastqc ", GEO_accession, "_2.fastq")
   }
+  
   else {
     fastqc_command = paste0("fastqc ", GEO_accession, ".fastq")
   }
+  
   rm_command = paste0("rm ", GEO_accession, ".sra")
+  
   paste(Wget_command, fastq_dump_command, rm_command, fastqc_command, sep = ";")
+  
 }
