c40cd761ebbfbbc3b364274f5073aebce7af15af
ZhenWei10 <zhen.wei10@icloud.com>
2018-02-03 17:56:52 +0800

    Correct fatal error
    default ignore strand = TRUE

--- R/Count_SRRs.R
+++ R/Count_SRRs.R
@@ -1,7 +1,12 @@
 #' @title Count data and save the result
 #' 
 #' @param SRRs The bam file names (without .bam).
-#' @param Mode,Inter.feature,Ignore.strand Default settings are "Union",FALSE, and FALSE resectively; see \code{\link{summarizeOverlaps}} for more information.
+#' @param Mode,Inter.feature,Ignore.strand Default settings are "Union",FALSE, and TRUE resectively; see \code{\link{summarizeOverlaps}} for more information.
+#' 
+#' Generally, set Ignore.strand = TRUE if you are not using strand specific library (other wise you usually lose half of your data).
+#' 
+#' Set Inter.feature = T if you want to deal with overlapping features.
+#' 
 #' @param reference_annotation A \code{\link{Granges}} object.
 #' @examples 
 #' 
@@ -15,7 +20,7 @@
 #' @export
 
 
-Count_SRRs <- function(SRRs,bam_dir = "./",reference_annotation,save_title,paired = F,Mode = "Union",Ignore.strand = F,Inter.feature = F) {
+Count_SRRs <- function(SRRs,bam_dir = "./",reference_annotation,save_title,paired = F,Mode = "Union",Ignore.strand = T,Inter.feature = F) {
   
   register(SerialParam())
   
